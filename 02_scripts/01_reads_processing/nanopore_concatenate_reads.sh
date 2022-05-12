a3f52a41f266ee37075439aab095a0a6297025a1
svannib <vanni.benvenga@gmail.com>
2022-05-12 14:28:58 +0200

    Commented concatenation script

--- 02_scripts/01_reads_processing/nanopore_concatenate_reads.sh
+++ 02_scripts/01_reads_processing/nanopore_concatenate_reads.sh
@@ -6,7 +6,6 @@
 
 #SBATCH --time=00:30:00        #This is the time that your task will run
 #SBATCH --qos=30min           #You will run in this queue
-#SBATCH --array=1-7
 
 # Paths to STDOUT or STDERR files should be absolute or relative to current working directory
 #SBATCH --output=slurm_nanopore_concatenate     #This is the joined STDOUT and STDERR file
@@ -22,22 +21,27 @@
 ml purge
 ml BBMap/38.91-foss-2018b
 
+# Declare working directory and sample batc as variables
 folder=/scicore/home/egliadr/GROUP/projects/food_pathogen_metagenomics_VB
 batch=library_prep_test_VB
 
+# Declare sample name and corresponding multiplexing barcode
 sample_id=(BS_Z1 BS_Z2 BS_Z3 CC_Z1 CC_Z2 CC_Z3 HMW)
 barcodes=(barcode02 barcode03 barcode04 barcode05 barcode06 barcode07 barcode08)
 
+#generate a new folder where the concatenated reads will be stored
 mkdir $folder/01_data/"$batch"/concatenated_reads/
 
+# Loop trough the samples
 for (( i=0; i<${#sample_id[@]}; i++ ))
-do echo "${sample_id[$i]}"
-echo "${barcodes[$i]}"
+do echo "Reads with "${barcodes[$i]}" concatenated in sample: "${sample_id[$i]}""
 
+# Generate a folder for each sample and concateate fastq reads there
 mkdir $folder/01_data/"$batch"/concatenated_reads/"${sample_id[$i]}"
 zcat $folder/01_data/"$batch"/fastq_pass/${barcodes[$i]}/*.gz \
 >> $folder/01_data/"$batch"/concatenated_reads/"${sample_id[$i]}"/"${sample_id[$i]}".fastq
 
+# Convert the .fastq file to a .fasta
 reformat.sh -Xmx4g in=$folder/01_data/"$batch"/concatenated_reads/"${sample_id[$i]}"/"${sample_id[$i]}".fastq \
 out=$folder/01_data/"$batch"/concatenated_reads/"${sample_id[$i]}"/"${sample_id[$i]}".fasta qin=33
 
